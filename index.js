e055ed2a6b8387b706a55aee6ce34e63b5d9252c
Jessica Oboh <jessicarichardson@Jessicas-MBP.lan>
2022-11-11 19:57:02 -0700

    added questions

--- index.js
+++ index.js
@@ -1,14 +1,73 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const { writeFile } = require('fs').promises;
+const generateMarkdown = require('./utils/generateMarkdown');
+const { writeToFile } = require('fs').promises;
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please enter a description of your project (What is it? Why and how did you make it?):",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What are the installation instructions for your project?",
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What instructions are needed to use your project?",
+
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Please select the license you used for this project:",
+        choices: [
+            "Apache 2.0",
+            "MIT",
+            "GNU GPL v3.0",
+            "BSD 3-Clause",
+            "OTHER",
+            "NONE",
+        ],
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Please enter the names of those who contributed to the project:",
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Was any testing done? If so, how?",
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "What is your GitHub username?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email?"
+    },
+];
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+    const answers = inquirer.prompt(questions);
+    generateMarkdown()
+      .then(() => writeToFile(readme.md, generateMarkdown()))
+      .catch((err) => console.error(err));
+  };
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,36 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { 
+  switch (license){
+    case "Apache 2.0":
+      return "https://opensource.org/licenses/Apache-2.0";
+    case "MIT":
+      return "https://opensource.org/licenses/MIT";
+    case "GNU GPL v3.0":
+      return "https://opensource.org/licenses/GPL-3.0";
+    case "BSD 3-Clause":
+      return "https://opensource.org/licenses/BSD-3-Clause";
+    case "OTHER":
+    case "NONE":
+        return "";
+    default:
+      break;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## Description
+  ${data.description}
 
 `;
 }
